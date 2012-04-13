68dae6262d21113232a8192a4d96076f9125d0b0
Marek Jędryka <marek629@poczta.onet.pl>
2012-04-14 01:01:28 +0200

    densing FFT

--- imagegenerator.cpp
+++ imagegenerator.cpp
@@ -57,16 +57,16 @@ void ImageGenerator::run()
             QSize size = img->size();
             size.setWidth( size.width() * extColumns * 2);
             *img = img->scaled(size);
-            short shift = height / (2*extColumns);
-            file->seek(height * fftRange.first + shift);
+            qint64 shift = height * fftRange.first + height / (2*extColumns); // wave file offset
             FFT::FFT fft(2 * height);
             double *buffer = new double [fft.bufferSize()];
             float j=0;
+            qDebug() << shift << height;
             for (int x=0; x<img->width(); x++) // FFT columns loop
             {
-                if (x%2 != 0)
+                if (x%(extColumns+1) != 0)
                 {
-                    file->readData(buffer,fft.bufferSize(),j*fft.bufferSize(),0);
+                    file->readData(buffer,fft.bufferSize(),shift+j*fft.bufferSize(),0);
                     fft.makeWindow(buffer);
                     fft.countFFT(buffer);
                     for (int y=height-1, k=0; y>=0; y--, k++)
@@ -74,6 +74,8 @@ void ImageGenerator::run()
                         uData = buffer[k] * 254;
                         if (uData > 255)
                             uData = 255;
+                        else if (uData < 0)
+                            uData = 0;
                         img->setPixel(x,y,colors->at(uData));
                     }
                     j += 0.5;
@@ -91,15 +93,17 @@ void ImageGenerator::run()
                     }
                 }
             }
+            delete [] buffer;
         }
         *img = img->scaled((zoomFactor_-extColumns)*img->width(), (zoomFactor_-extColumns)*img->height(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
-        img->save("/home/marek/img.png");
-        //qDebug() << "Time:" << time.elapsed() << fftRange.first << img->width() << fftSamples;
+//        img->save("/home/marek/img.png");
+        qDebug() << "Time:" << time.elapsed() << fftRange.first;
     }
 }
 
 QImage * ImageGenerator::plotImage(int startFFT, int stopFFT)
 {
+    qDebug() << startFFT << stopFFT;
     ImageData d;
     d.fftRange.first = startFFT;
     d.fftRange.second = stopFFT;
